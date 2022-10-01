02d9325c0fe28136ef90de775ebaf1ce66ba3431
soaresgabe <gabesoares@proton.me>
2022-10-01 09:45:30 -0300

    Day 02

---
+++ 07 - React Components/src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <h1>Things I Like</h1>;
+}
+
+export default Heading;
---
+++ 07 - React Components/src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Coffee</li>
+      <li>Cuddling</li>
+      <li>Coding</li>
+    </ul>
+  );
+}
+
+export default List;
