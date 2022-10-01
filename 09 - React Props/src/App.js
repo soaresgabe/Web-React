11982dc6278dbc96295035130deadcce71091319
soaresgabe <gabesoares@proton.me>
2022-10-01 14:12:25 -0300

    Day 02

---
+++ 09 - React Props/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        alt="avatar_img"
+        num="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        num="+7387384587"
+        email="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        alt="avatar_img"
+        num="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
+    </div>
+  );
+}
+
+export default App;
